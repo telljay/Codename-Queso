93a52caaf0594bb532512df754585edf7054f16a
telljay <toellja2221@gmail.com>
2024-04-29 12:46:11 -0500

    beginning on the upgraded searches

--- python_files/devQs.py
+++ python_files/devQs.py
@@ -1,25 +1,35 @@
 import sqlite3
 from sqlite3 import Error
-def basicSearch(conn):
+def basicSearch(conn, checkBoxResults, userInput):
     print(f"Please enter a search: \n")
-    word = input()
+
     basicSearch_query = """
     SELECT Name
-    FROM Cheese
+    FROM ?
     WHERE Name LIKE ? COLLATE NOCASE;
     """
     cursor = conn.cursor()
-    cursor.execute(basicSearch_query,("%"+word+"%",))
+    cursor.execute(basicSearch_query,(checkBoxResults,"%"+userInput+"%"))
     rows = cursor.fetchall()
     for row in rows:
         print(f"{row[0]}")
 
     
+def vendorSearch(conn):
+    vendorSearch_query = """
+    SELECT Name
+    FROM Vendor
+    WHERE Name = ?
+    """
+
+
 def main():
     conn = None
     try:
         conn = sqlite3.connect("C:/Users/toell/Desktop/Codename Queso/Queso Database.db")
         conn.row_factory = sqlite3.Row
+        
+
         
         basicSearch(conn)
 
