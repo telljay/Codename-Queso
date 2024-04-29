9ee44987663201ef9c954cd831ee58b19f651a6d
john <johnpcargille@gmail.com>
2024-04-29 11:01:41 -0500

    flask-main can now perform basic search in a test environment

--- python_files/main_with_flask.py
+++ python_files/main_with_flask.py
@@ -1,7 +1,3 @@
-#CURRENTLY BROKEN
-#CURRENTLY BROKEN
-#CURRENTLY BROKEN
-
 import sqlite3
 from sqlite3 import Error
 
@@ -19,21 +15,28 @@ def simpleSearch():
     try:
         jsonPostData = request.get_json()
         searchInput = jsonPostData["userInput"]
-        # conn = sqlite3.connect("C:/Users/toell/Desktop/Codename Queso/Queso Database.db")
-        # conn.row_factory = sqlite3.Row
+        # Might alternatively use a request.form["searchInput"]
+
+        # Change this to be your path
+        conn = sqlite3.connect("/mnt/c/Users/saxop/SoftwareDesign/Codename-Queso/Queso Database.db")
+        conn.row_factory = sqlite3.Row
         
         print(f"Please enter a search: \n")
         word = searchInput
-        # basicSearch_query = """
-        # SELECT Name
-        # FROM Cheese
-        # WHERE Name LIKE ? COLLATE NOCASE;
-        # """
-        # cursor = conn.cursor()
-        # cursor.execute(basicSearch_query,("%"+word+"%",))
-        # rows = cursor.fetchall()
-        # return rows
-        return jsonPostData["userInput"]
+        basicSearch_query = """
+        SELECT Name
+        FROM Cheese
+        WHERE Name LIKE ? COLLATE NOCASE;
+        """
+        
+        results = []
+        cursor = conn.cursor()
+        cursor.execute(basicSearch_query,("%"+word+"%",))
+        rows = cursor.fetchall()
+        for row in rows :
+            row_dict = {"Name": row[0]}
+            results.append(row_dict)
+        return results
 
     except Error as e:
         print(f"Error opening the database {e}")
